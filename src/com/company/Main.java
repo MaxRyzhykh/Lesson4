f7f87b2bedc38e5a3e7313256b01c134092b2ee6
MaxRyzhykh <maxim.ryzhykh@gmail.com>
2020-10-21 23:32:09 +0300

    first commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,30 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	Rectangle rectangle_1 = new Rectangle(3);
+
+	Rectangle rectangle_2 = new Rectangle(3, 4);
+
+	Rectangle rectangle_3 = new Rectangle(4, 3);
+
+	System.out.println(rectangle_1.x + " " + rectangle_1.y);
+	System.out.println(rectangle_1.CalculateArea());
+	rectangle_1.PrintArea();
+	rectangle_1.PrintRectangleKind();
+	System.out.println(rectangle_1.IsTheSameRectangle(rectangle_2));
+
+		System.out.println();
+	System.out.println(rectangle_2.x + " " + rectangle_2.y);
+	System.out.println(rectangle_2.CalculateArea());
+	rectangle_2.PrintArea();
+	rectangle_2.PrintRectangleKind();
+	System.out.println(rectangle_2.IsTheSameRectangle(rectangle_3));
+
+		System.out.println();
+	System.out.println(rectangle_3.x + " " + rectangle_3.y);
+	System.out.println(rectangle_3.CalculateArea());
+	rectangle_3.PrintArea();
+	rectangle_3.PrintRectangleKind();
+	System.out.println(rectangle_3.IsTheSameRectangle(rectangle_3));
     }
 }
---
+++ src/com/company/Rectangle.java
@@ -0,0 +1,53 @@
+package com.company;
+
+import java.awt.geom.Area;
+
+public class Rectangle {
+    double x;
+    double y;
+
+//1й: конструктор
+    Rectangle (double sx, double sy) {
+        x = sx;
+        y = sy;
+    }
+
+//2й: конструктор вадрата
+    Rectangle (double syx) {
+        x = syx;
+        y = syx;
+    }
+
+//1й: метод
+     String CalculateArea() {
+        String Area = String.valueOf((x*y));
+
+        return Area;
+    }
+
+//2й: метод
+    void PrintArea() {
+        String StringArea = CalculateArea();
+
+        System.out.println("S = " + StringArea);
+    }
+
+//3й: метод
+    void PrintRectangleKind() {
+        if (x == y) {
+            System.out.println("Это квадрат");
+        } else if (x != y) {
+            System.out.println("Это прямоугольник");
+        } else if (x<0 | y<0) {
+            System.out.println("Ошибка данных, введено отрицательно число");
+        }
+    }
+
+//4й: метод
+    boolean IsTheSameRectangle(Rectangle rectangle) {
+        return x == rectangle.x &&
+                y == rectangle.y ||
+               x == rectangle.y &&
+                y == rectangle.x;
+    }
+}
